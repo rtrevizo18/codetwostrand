60701df1cce860c76b61ac745353072f7dcdcea1
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-05-21 20:30:01 -0500

    feat: add footer components and modify splash with WIP body
    Just pushing these unfinished changes to push for new computer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import styles from './App.module.css';
 
 import Header from './components/Header';
 import Splash from './components/Splash';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className={styles.body}>
       <Header />
       <Splash />
+      <Footer />
     </div>
   );
 }
--- client/src/components/Splash.jsx
+++ client/src/components/Splash.jsx
@@ -20,6 +20,25 @@ function MagGlassSVG({ sizeRatio = 1.0 }) {
   );
 }
 
+const DESKTOP_WIDTH = 16;
+const DESKTOP_HEIGHT = 16;
+
+function DesktopSVG({ sizeRatio = 1.0 }) {
+  const width = DESKTOP_WIDTH * sizeRatio;
+  const height = DESKTOP_HEIGHT * sizeRatio;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="currentColor"
+      class="bi bi-pc-display"
+      viewBox="0 0 16 16">
+      <path d="M8 1a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1zm1 13.5a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0m2 0a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0M9.5 1a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM9 3.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1h-5a.5.5 0 0 0-.5.5M1.5 2A1.5 1.5 0 0 0 0 3.5v7A1.5 1.5 0 0 0 1.5 12H6v2h-.5a.5.5 0 0 0 0 1H7v-4H1.5a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5H7V2z" />
+    </svg>
+  );
+}
+
 function SearchForm() {
   const handleSubmit = event => {
     event.preventDefault();
@@ -34,18 +53,47 @@ function SearchForm() {
   );
 }
 
-function Splash() {
+function WhatWeDo() {
   return (
-    <div className={styles.splash_body}>
-      <img src={codingGIF} alt="Image of code running" />
-      <div className={styles.splash_gif_foreground}>
-        <h1>
-          Connecting,
-          <br /> one strand at a time
-        </h1>
-        <SearchForm />
+    <div className={styles.what_we_provide}>
+      <h2>What We Provide</h2>
+      <div className={styles.what_we_provide_cards}>
+        <div>
+          <DesktopSVG sizeRatio={3.0} />
+          <div>note</div>
+        </div>
+        <div>
+          <img></img>
+          <div>note</div>
+        </div>
+        <div>
+          <img></img>
+          <div>note</div>
+        </div>
+        <div>
+          <img></img>
+          <div>note</div>
+        </div>
       </div>
     </div>
+  );
+}
+
+function Splash() {
+  return (
+    <>
+      <div className={styles.splash_body_main}>
+        <img src={codingGIF} alt="Image of code running" />
+        <div className={styles.splash_gif_foreground}>
+          <h1>
+            Connecting,
+            <br /> one strand at a time
+          </h1>
+          <SearchForm />
+        </div>
+      </div>
+      <WhatWeDo />
+    </>
   );
 }
 
