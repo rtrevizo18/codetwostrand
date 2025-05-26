83f8be7e946d88329b8d67fcdff59b287f6af7b0
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-05-26 12:00:51 -0500

    feat: modify body for semantic and responsiveness, header links to right
    Header was changed for links to be on the right to match other similar website styles,
    body was edited to be more responsiveness, including adding new semantic
    tags and changing flex properties. STILL WIP since body behavior with height
    is not expected, probably something to do using vh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import styles from './App.module.css';
 
 import Header from './components/Header';
 import Splash from './components/Splash';
+import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className={styles.body}>
       <Header />
-      <Splash />
+      <main>
+        <Splash />
+      </main>
       <Footer />
     </div>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,24 +4,26 @@ import LogoSVG from './Logo';
 function Header() {
   return (
     <header className={styles.header}>
-      <nav>
-        <div className={styles.header_logo}>
-          <button>
-            <LogoSVG sizeRatio={0.4} />
-          </button>
-          <button>
-            <div className={styles.header_title}>Code2Strand</div>
-          </button>
-        </div>
-        <a href="">Support</a>
-        <a href="">Projects</a>
-        <a href="">Links</a>
-        <a href="">Links</a>
-      </nav>
-      <div className={styles.header_buttons}>
-        <button>Sign Up</button>
-        <button>Log In</button>
+      <div className={styles.header_logo}>
+        <button>
+          <LogoSVG sizeRatio={0.4} />
+        </button>
+        <button>
+          <div className={styles.header_title}>Code2Strand</div>
+        </button>
       </div>
+      <nav>
+        <div className={styles.nav_links}>
+          <a href="">Support</a>
+          <a href="">Projects</a>
+          <a href="">Links</a>
+          <a href="">Links</a>
+        </div>
+        <div className={styles.nav_buttons}>
+          <button>Sign Up</button>
+          <button>Log In</button>
+        </div>
+      </nav>
     </header>
   );
 }
