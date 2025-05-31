f4ee52a7de88d5cc935a4eb287a3ca5b01d9710e
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-05-31 08:48:43 -0500

    refactor: add feature dir, move aboutus/splash, edit dir locations to fix
    Also change 'CodingSplash1.gif' to 'CodingSplash.gif' and delete original 'CodingSplash.gif'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import styles from './App.module.css';
 
 import Header from './components/Header';
-import Splash from './components/Splash';
-import AboutUs from './components/AboutUs';
+import Splash from './features/splash/Splash';
+import AboutUs from './features/aboutus/AboutUs';
 import Footer from './components/Footer';
 
 function App() {
--- client/src/features/splash/svgs/Svg.jsx
+++ client/src/features/splash/svgs/Svg.jsx
@@ -1,6 +1,3 @@
-import styles from './Splash.module.css';
-import codingGIF from '../assets/CodingSplash1.gif';
-
 const MAG_GLASS_SVG_WIDTH = 16;
 const MAG_GLASS_SVG_HEIGHT = 16;
 
@@ -97,64 +94,4 @@ function PersonSVG({ sizeRatio = 1.0 }) {
   );
 }
 
-function SearchForm() {
-  const handleSubmit = event => {
-    event.preventDefault();
-  };
-  return (
-    <form className={styles.search_form_main} onSubmit={handleSubmit}>
-      <input type="text" placeholder="Look for your next project..."></input>
-      <button type="submit">
-        <MagGlassSVG />
-      </button>
-    </form>
-  );
-}
-
-const SIZERATIO = 3.0;
-
-function WhatWeDo() {
-  return (
-    <div className={styles.what_we_provide}>
-      <h2>What We Provide</h2>
-      <div className={styles.what_we_provide_cards}>
-        <div className={styles.what_we_provide_card}>
-          <DesktopSVG sizeRatio={SIZERATIO} />
-          <div>IT Work, remote or in-person</div>
-        </div>
-        <div className={styles.what_we_provide_card}>
-          <CodeSVG sizeRatio={SIZERATIO} />
-          <div>Coding solutions, for our own</div>
-        </div>
-        <div className={styles.what_we_provide_card}>
-          <LightbulbSVG sizeRatio={SIZERATIO} />
-          <div>Any software idea!</div>
-        </div>
-        <div className={styles.what_we_provide_card}>
-          <PersonSVG sizeRatio={SIZERATIO} />
-          <div>Let us know, we might just be able to help!</div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Splash() {
-  return (
-    <>
-      <div className={styles.splash_body_main}>
-        <img src={codingGIF} alt="Image of code running" />
-        <div className={styles.splash_gif_foreground}>
-          <h1>
-            Connecting,
-            <br /> one strand at a time
-          </h1>
-          <SearchForm />
-        </div>
-      </div>
-      <WhatWeDo />
-    </>
-  );
-}
-
-export default Splash;
+export default { MagGlassSVG, DesktopSVG, CodeSVG, LightbulbSVG, PersonSVG };
