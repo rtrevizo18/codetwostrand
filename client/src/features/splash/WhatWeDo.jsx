3e5322f4266db69e875e50104cbe018fe9590747
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-05-31 14:01:39 -0500

    feat(splash): modify WhatWeDo cards in order of priority

--- client/src/features/splash/WhatWeDo.jsx
+++ client/src/features/splash/WhatWeDo.jsx
@@ -4,33 +4,34 @@ import { DesktopSVG, CodeSVG, LightbulbSVG, PersonSVG } from './svgs/Svg';
 
 const SIZERATIO = 3.0;
 
-function CodingSolutions() {
+function SoftwareSolutions() {
   return <div>Hey</div>;
 }
 
 function WhatWeDo() {
+  const [cardState, updateCardState] = useState(0);
   return (
     <div className={styles.what_we_provide}>
       <h2>What We Provide</h2>
       <div className={styles.what_we_provide_cards}>
         <div className={styles.what_we_provide_card}>
-          <DesktopSVG sizeRatio={SIZERATIO} />
-          <div>IT Work, remote or in-person</div>
+          <LightbulbSVG sizeRatio={SIZERATIO} />
+          <div>Any software idea!</div>
         </div>
         <div className={styles.what_we_provide_card}>
           <CodeSVG sizeRatio={SIZERATIO} />
-          <div>Coding solutions, for our own</div>
+          <div>Help with Coding?</div>
         </div>
         <div className={styles.what_we_provide_card}>
-          <LightbulbSVG sizeRatio={SIZERATIO} />
-          <div>Any software idea!</div>
+          <DesktopSVG sizeRatio={SIZERATIO} />
+          <div>IT Work, remote or in-person</div>
         </div>
         <div className={styles.what_we_provide_card}>
           <PersonSVG sizeRatio={SIZERATIO} />
           <div>Let us know, we might just be able to help!</div>
         </div>
       </div>
-      <CodingSolutions />
+      <SoftwareSolutions />
     </div>
   );
 }
